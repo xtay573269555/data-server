6ebec417db0e0e8ae98d94564499cc73aa01a297
rensuiyi <ren084773820@gmail.com>
2015-12-22 15:51:27 +0800

    add support for device test

--- app/views.py
+++ app/views.py
@@ -3,7 +3,20 @@
 
 from app import app
 from flask import render_template
+from flask import request
+from flask import json,jsonify
+import time
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+@app.route('/device',methods=['POST'])
+def device():
+    print request.json
+    bak = {
+        'at':'14646464',
+        'code':'200'
+    }
+    bak['at']=str(int(time.time()))
+    return  jsonify(bak),200
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from app import app
 
 manager = Manager(app)
 manager.add_command("runserver", 
-        Server(host="127.0.0.1", port=5000, use_debugger=True))
+        Server(host="0.0.0.0", port=21050, use_debugger=True))
 
 if __name__ == '__main__':
     manager.run()
